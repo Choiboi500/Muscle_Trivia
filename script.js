2274b94026202ead14b194a4d1f561f173493dd8
Choiboi500 <jbc8894@gmail.com>
2020-07-10 14:04:55 -0400

    array and start button started

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 // make data-base of questions
-const elbowFlexion = `Action: Elbow Flexion`;
-const elbowExtension = `Action: Elbow Extension`;
-const kneeFlexion = `Action: Knee Flexion`;
-const kneeExtension = `Action: Knee Extension`;
-const hipFlexion = `Action: Hip Flexion`;
-const hipExtension = `Action: Hip Extension`;
-const anteriorShoulderFlexion = `Action: Anterior Shoulder Flexion`;
-const lateralShoulderFlexion = `Action: Frontal Humeral Abduction`;
-const scapularElevation = `Action: Scapular Elevation`;
-const breathing = `Action: Breathing`;
-const scapularRotation = `Action: External Scapular Rotation`;
-const chestFlexion = `Action: Horizontal Humeral Adduction`;
+let questions = [`Action: Elbow Flexion`,
+ `Action: Elbow Extension`,
+`Action: Knee Flexion`,
+ `Action: Knee Extension`,
+ `Action: Hip Flexion`,
+`Action: Hip Extension`,
+ `Action: Anterior Shoulder Flexion`,
+`Action: Frontal Humeral Abduction`,
+`Action: Scapular Elevation`,
+ `Action: Breathing`,
+`Action: External Scapular Rotation`,
+ `Action: Horizontal Humeral Adduction`,]
 
 // make data-base of answers
-const biceps = `BICEPS`;
-const triceps = `TRICEPS`;
-const hamstrings = `HAMSTRINGS`;
-const quadriceps = `QUADRICEPS`;
-const ilioPsoas = `ILIOPSOAS`;
-const gluteusMaximus = `GLUTEUS MAXIMUS`;
-const anteriorDeltoid = `ANTERIOR DELTOID`;
-const lateralDeltoid = `LATERAL DELTOID`;
-const trapezius = `TRAPEZIUS`;
-const diaphragm = `DIAPHRAGM`;
-const latissimusDorsi = `LATISSIMUS DORSI`;
-const pectoralis = `PECTORALIS`;
+ const biceps = `BICEPS`,
+ const triceps = `TRICEPS`,
+ const hamstrings = `HAMSTRINGS`,
+ const quadriceps = `QUADRICEPS`,
+ const ilioPsoas = `ILIOPSOAS`,
+ const gluteusMaximus = `GLUTEUS MAXIMUS`,
+ const anteriorDeltoid = `ANTERIOR DELTOID`,
+ const lateralDeltoid = `LATERAL DELTOID`,
+ const trapezius = `TRAPEZIUS`,
+ const diaphragm = `DIAPHRAGM`,
+ const latissimusDorsi = `LATISSIMUS DORSI`,
+ const pectoralis = `PECTORALIS`,
+
+ //create array variable for answers and current answer to start game
+ let answers = []
+ let currentAnswer = 0;
+
 
 // make start button clicked change h1 to question, create input section, and create submit button
-
+const startButton = document.querySelector('.startButton')
+startButton
 // make any input change toUpperCase after Submit is clicked
+
+// create function to random draw from array
 
 // if I get an answer right, store it
 // if I don't get an answer right, don't store it
